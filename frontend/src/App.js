fa52aa8f1b8cef757e06ef31384c865b68374df7
AnkitSinghRawat1 <ankitintchar@gmail.com>
2022-10-06 13:21:53 +0530

    Protected Routes Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { EditProfile, Login } from './Pages'
+import { EditProfile, Login } from "./Pages";
+import ProtectedRoutes from "./utility/ProtectedRoutes/ProtectedRoutes";
 
- function App() {
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route path="/editProfile" element={<EditProfile />} />
+
+          <Route path="/editProfile" element={<ProtectedRoutes />}>
+            <Route path="/editProfile" element={<EditProfile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
